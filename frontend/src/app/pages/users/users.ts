335f3fdf672eabe75280eef18f9a3ea5e24d332e
Medidhi V Naga Sri Surya Satya Santhoshi <23221a0576@bvcgroup.in>
2026-09-01 21:14:34 +0530

    Delete frontend directory

--- frontend/src/app/pages/users/users.ts
+++
@@ -1,8 +0,0 @@
-import { Component, OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { HttpClient } from '@angular/common/http';
-import { MatCardModule } from '@angular/material/card';
-import { MatTableModule } from '@angular/material/table';
-@Component({selector:'app-users',standalone:true,imports:[CommonModule,MatCardModule,MatTableModule],templateUrl:'./users.html',styleUrl:'./users.css'})
-export class UsersComponent implements OnInit { users:any[]=[]; cols=['full_name','email','role']; constructor(private http:HttpClient){} ngOnInit(){this.http.get<any[]>('https://vendor-reliability-intelligence-platform-2h9h.onrender.com/users/').subscribe({next:d=>this.users=d,error:e=>console.error(e)});}}
-
--- frontend/src/environments/environment.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: false,
-  apiUrl: 'https://vendor-reliability-intelligence-platform-2h9h.onrender.com',
-};
